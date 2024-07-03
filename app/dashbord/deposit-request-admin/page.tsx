4f51560ffb34842fe139302bb3cd226d137b11dc
mainbsl4 <mainbsl4@gmail.com>
2024-07-03 12:54:48 +0600

    main

--- app/dashbord/deposit-request-admin/page.tsx
+++ app/dashbord/deposit-request-admin/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Deposit_request_admin_Table from '../../components/dashbord/deposit-request-admin/Deposit_request_admin_Table'
+import React from "react";
+import Deposit_request_admin_Table from "../../components/dashbord/deposit-request-admin/Deposit_request_admin_Table";
 
 export default function page() {
   return (
     <div>
-        <Deposit_request_admin_Table/>
+      <Deposit_request_admin_Table />
     </div>
-  )
+  );
 }
