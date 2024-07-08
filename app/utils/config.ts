b45e7b0ac605fe16a6b26f9fa2516074ccb6a873
asifullesan <asifullesan345@gmail.com>
2024-07-09 00:13:55 +0530

    dghgdh
    dghgdh

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,6 +1,6 @@
 
-export const base_url = "https://nanofirst.onrender.com/api/"
-// export const base_url = "http://localhost:10000/api/"
+// export const base_url = "https://nanofirst.onrender.com/api/"
+export const base_url = "http://localhost:10000/api/"
 
 
 
