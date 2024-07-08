8a055c5572f650e844e34bbc715b2b2c734fc0d7
mainbsl4 <mainbsl4@gmail.com>
2024-07-08 23:55:15 +0600

    main

--- app/dashbord/deposit-request/page.tsx
+++ app/dashbord/deposit-request/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Deposit_request_from from "../../components/dashbord/deposit-request/Deposit_request_from";
-import Deposit_request_table from "../../components/dashbord/deposit-request/Deposit_request_table";
+// import Deposit_request_table from "../../components/dashbord/deposit-request/Deposit_request_table";
 
 export default function page() {
   return (
@@ -8,7 +8,7 @@ export default function page() {
       <div className="flex justify-center items-center mb-4">
         <Deposit_request_from />
       </div>
-      <Deposit_request_table />
+      {/* <Deposit_request_table /> */}
     </div>
   );
 }
---
+++ app/dashbord/lone-request-list/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Lone_request_table from '../../components/dashbord/lond-request/Lone_request_table'
+
+export default function page() {
+  return (
+    <div>
+        <Lone_request_table/>
+    </div>
+  )
+}
