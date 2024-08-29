4343686ac8a2cac3f2c11e5dfc797211ad292b20
mainbsl4 <mainbsl4@gmail.com>
2024-08-29 17:48:33 +0600

    main

---
+++ app/email_validation/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+
+export default function Page() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `http://localhost:10000/api/user/emailverify/${email}`
+      );
+      console.log("Success:", response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <section className="bg-gray-50 bgImgSignin">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Sign in to your account
+            </h1>
+            <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Email:
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-500 text-white p-2 rounded-lg"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
